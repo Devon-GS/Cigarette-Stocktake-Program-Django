44644e9dd1a68e51c1e8ec0b8fb71470d5f39bcd
Devon <ds.pydev@gmail.com>
2022-04-06 20:21:05 +0200

    Deleted Static Files

--- stocktake/settings.py
+++ stocktake/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'stocktake.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
 
